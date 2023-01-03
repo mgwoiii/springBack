ac1c255dde0a92a10a3807d5cd7ae0ed093ca115
mgwoiii33 <mgwoiii33@trgtech.co.kr>
2023-01-03 17:24:32 +0900

    c

---
+++ src/main/java/com/core/springback/SpringBackApplication.java
@@ -0,0 +1,14 @@
+package com.core.springback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBackApplication.class, args);
+        //
+    }
+
+}
---
+++ src/test/java/com/core/springback/SpringBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.springback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
